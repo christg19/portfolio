8cbd7be0fbad3c0ab2f6f9e6f1889182785affbd
Christgb <srsteve1913@gmail.com>
2022-04-04 06:15:52 -0400

    portafolio

---
+++ js/cv.js
@@ -0,0 +1,47 @@
+const Choreographer = require('choreographer-js')
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  });
+
+  function toggle_light_mode() {
+    var app = document.getElementsByTagName("BODY")[0];
+    if (localStorage.lightMode == "dark") {
+        localStorage.lightMode = "light";
+        app.setAttribute("light-mode", "light");
+    } else {
+        localStorage.lightMode = "dark";
+        app.setAttribute("light-mode", "dark");
+    }
+}
+
+window.addEventListener(
+    "storage",
+    function () {
+        if (localStorage.lightMode == "dark") {
+            app.setAttribute("light-mode", "dark");
+        } else {
+            app.setAttribute("light-mode", "light");
+        }
+    },
+    false
+);
+
+
+let notificacion = anime.timeline({
+    targets: '.visually-hidden',
+    autoplay: true,
+    direction: 'alternate',
+    loop: true
+  });
